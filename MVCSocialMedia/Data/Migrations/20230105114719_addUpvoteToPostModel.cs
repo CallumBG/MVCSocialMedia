b79097c20fb2ca233601bfe235b2522f815fa1c3
Callum Busuttil-Goodfellow <callumbusuttilgoodfellow@gmail.com>
2023-01-05 12:08:28 +0000

    fixed home page links

---
+++ MVCSocialMedia/Data/Migrations/20230105114719_addUpvoteToPostModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCSocialMedia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUpvoteToPostModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UpvoteCount",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpvoteCount",
+                table: "Posts");
+        }
+    }
+}
